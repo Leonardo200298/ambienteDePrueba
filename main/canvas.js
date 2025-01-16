2f3cc1d6880889bea54f9503915c38875fcb0e3d
Leonardo <leonardosanchez119@gmail.com>
2025-01-16 12:09:27 -0300

    refuerzo de formas en canvas

---
+++ main/canvas.js
@@ -0,0 +1,7 @@
+let canvas = document.querySelector("#canvas")
+let ctx = canvas.getContext("2d")
+ctx.fillStyle = "rgb(133, 133, 1)";
+//           x    y width height
+ctx.fillRect(10, 10, 55, 50);
+ctx.fillStyle = "rgb(241, 241, 0)"
+ctx.fillRect(15,15,55,50)
